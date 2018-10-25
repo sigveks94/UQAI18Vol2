59b7f20638bb46c68fb4b14bcd82c29de93c776d
erlendhjellestrandkleiv <erlendhjellestrandkleiv@erlends-mbp>
2018-10-25 17:35:33 +1000

    first push

---
+++ src/mdp_solver/MDPSolver.java
@@ -0,0 +1,34 @@
+package mdp_solver;
+
+import problem.*;
+import simulator.*;
+
+public class MDPSolver {
+	
+	
+	
+	
+	
+	
+	
+	/*
+	 * THOUGTHS OF WHAT TO DO
+	 * 
+	 * 
+	 * 1.	Generate neigbour-states to current state
+	 * 2.	Generate reward to a given state (both deterministic and non-deterministic)
+	 * 3.	Transition-function (both deterministic and non-deterministic)
+	 * 4.	Create a utility function that multiplies transition and reward
+	 * 5.	Policy iteration
+	 * 6.	Find optimal actions according to policy iteration
+	 * 7.	Return string of resulting optimal actions
+	 * 8.	ant-jar build file
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
+
+
+}
